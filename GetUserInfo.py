66b36702bca247c07f50cb5fc7f852e36979bf12
Austin <austin8432@gmail.com>
2021-10-29 17:01:54 +0800

    add TextCtrl

--- GetUserInfo.py
+++ GetUserInfo.py
@@ -1,6 +1,6 @@
 from typing import Container, Text
 import wx
-from wx.core import EVT_BUTTON, TextCtrl
+from wx.core import EVT_BUTTON, Colour, TextCtrl
 
 
 class GetUserInfo(wx.Frame):
@@ -10,8 +10,16 @@ class GetUserInfo(wx.Frame):
 
     def InitUI(self):
         self.panel = wx.Panel(self) 
-        self.btn = wx.Button(self.panel, label ="Click", pos =(75, 10)) 
-        self.btn.Bind(wx.EVT_BUTTON, self.Onclick) 
+        # self.sizer = wx.BoxSizer(wx.VERTICAL)
+        self.label = wx.StaticText(self.panel, label = "HELLO", pos = (5, 10))
+        self.btn = wx.Button(self.panel, label ="Click", pos = (5, 30)) 
+        self.txtfield = wx.TextCtrl(self.panel, pos = (5, 60))
+        self.btn.Bind(wx.EVT_BUTTON, self.Onclick)
+
+        
+        # wx.SizerFlags().Border(wx.TOP|wx.LEFT, 25)
+        # self.sizer.Add(self.label)
+        # self.sizer.Add(self.btn)
 
         self.SetMinSize((400, 250)) 
         self.Centre() 
@@ -20,6 +28,7 @@ class GetUserInfo(wx.Frame):
     def Onclick(self, event):
         # SET A STRING LABEL FOR BUTTON 
         self.btn.SetLabel("Clicked") 
+        self.txtfield.SetLabel("HI")
 
 ex = wx.App() 
 GetUserInfo(None, 'Get User Infromation') 
@@ -30,13 +39,11 @@ ex.MainLoop()
 # frm = wx.Frame(None, title="Hello World!")
 # panel = wx.Panel(frm)
 # sizer = wx.BoxSizer(wx.VERTICAL)
-# # sizer.Add(label2, wx.SizerFlags.Border(wx.TOP|wx.LEFT, 25))
-# # label = wx.StaticText(panel, label = "Hello World", pos = (100,50)) 
+# label = wx.StaticText(panel, label = "Hello World", pos = (100,50)) 
 # label2 = wx.StaticText(panel, label = "Hello World")
 # button = wx.Button(panel, label = "TEST")
-# button.Bind(wx.EVT_BUTTON, self.Onclick())
 
-# #wx.SizerFlags().Border(wx.TOP|wx.LEFT, 25)
+# wx.SizerFlags().Border(wx.TOP|wx.LEFT, 25)
 # sizer.Add(label2)
 # sizer.Add(button)
 # panel.SetSizer(sizer)
